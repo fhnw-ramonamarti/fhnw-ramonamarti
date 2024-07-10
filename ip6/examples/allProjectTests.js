fbfa3453cc98d5a21acbf63617365d9098df3948
Ramona Marti <ramona.marti@students.fhnw.ch>
2024-07-10 16:29:08 +0200

    update log, add test page, update links

---
+++ ip6/examples/allProjectTests.js
@@ -0,0 +1,8 @@
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/optionsModelTest.js";
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/optionsControllerTest.js";
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/columnOptionsProjectorTest.js";
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/columnOptionsComponentTest.js";
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectControllerTest.js";
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectProjectorTest.js";
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectComponentTest.js";
+// import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/interactionProjectorTest.js";
