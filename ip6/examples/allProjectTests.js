79f1bcb6debb733c8b18ea60629f3af633b030dc
Ramona Marti <ramona.marti@students.fhnw.ch>
2024-08-01 20:06:06 +0200

    update index links

--- ip6/examples/allProjectTests.js
+++ ip6/examples/allProjectTests.js
@@ -4,5 +4,6 @@ import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectC
 import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/columnOptionsComponentTest.js";
 import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectControllerTest.js";
 import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectProjectorTest.js";
+import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectProjectorTestAsync.js";
 import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectComponentTest.js";
 // import "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/interactionProjectorTest.js";
