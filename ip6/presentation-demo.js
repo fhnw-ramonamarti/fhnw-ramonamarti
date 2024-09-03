c7e780a769e3523c3ea816e7d219a1640483b86d
Ramona Marti <ramona.marti@students.fhnw.ch>
2024-09-03 15:45:49 +0200

    add demo files for pres

---
+++ ip6/presentation-demo.js
@@ -0,0 +1,6 @@
+import {
+    SelectComponentByCallbacks as SelectComponent,
+    pageCss,
+} from "https://fhnw-ramonamarti.github.io/Kolibri/src/kolibri/projector/selectComponent/selectComponent.js";
+import * as Service from "./userTest/dataService.js";
+
